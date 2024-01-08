d11789739151e862298527f7cae77c94e027ce28
Danqing Liu <git@danqing.io>
2024-01-08 16:19:43 -0500

    Update PresentationHostingWindow.swift

--- Sources/Transmission/Sources/Hosting/PresentationHostingWindow.swift
+++ Sources/Transmission/Sources/Hosting/PresentationHostingWindow.swift
@@ -18,6 +18,7 @@ open class PresentationHostingWindow<Content: View>: UIWindow {
     public init(windowScene: UIWindowScene, content: Content) {
         self.host = PresentationHostingWindowController(content: content)
         super.init(windowScene: windowScene)
+        host.view.backgroundColor = .clear
         rootViewController = host
     }
 
