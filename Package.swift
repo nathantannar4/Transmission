1e8ef556ada10e996e7aa1ef94d723ce1091a411
Danqing Liu <git@danqing.io>
2024-01-08 15:33:27 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/nathantannar4/Engine", from: "1.4.0"),
+        .package(url: "https://github.com/nathantannar4/Engine", from: "1.3.0"),
         .package(url: "https://github.com/nathantannar4/Turbocharger", from: "1.0.1"),
     ],
     targets: [
