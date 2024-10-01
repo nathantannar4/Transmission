0ce511345b8c805b981780b64284b67d79b50c78
Nathan Tannar <nathantannar4@gmail.com>
2024-09-30 20:18:45 -0700

    1.4.10

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/nathantannar4/Engine", from: "1.8.5"),
+        .package(url: "https://github.com/nathantannar4/Engine", from: "1.8.6"),
         .package(url: "https://github.com/nathantannar4/Turbocharger", from: "1.3.1"),
     ],
     targets: [
